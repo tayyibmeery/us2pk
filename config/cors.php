0c6fa1dbf8fde503830189938df76bb7a7edddd1
tayyib meery <tayyibmeerybalti@gmail.com>
2026-09-21 01:00:57 +0500

    hshsh

--- config/cors.php
+++ config/cors.php
@@ -10,3 +10,29 @@ return [
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
+
+
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://us2pk.com',
+        'https://www.us2pk.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
